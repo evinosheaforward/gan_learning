01ac9ebabf04f5b30defb757bc3c66d0aebb7bbd
Evin O'Shea <evinosheaforward@gmail.com>
2021-03-20 21:13:36 -0400

    create larger latent space input model, break up training into iterations on each half of the gan, training both at once does bad things

--- gan/playground.py
+++ gan/playground.py
@@ -1,3 +1,4 @@
+import os
 import timeit
 
 import matplotlib.pyplot as plt
@@ -13,45 +14,32 @@ GPU_DEVICE = torch.device("cuda")  # Default CUDA device
 
 
 def main():
-    if True:
-        # Try out the generator model with random input
-        latent_space_samples = torch.randn(1, 1, 28, 28, device=GPU_DEVICE)
-    else:
-        # see what the model does with real input
-        latent_space_samples = next(
-            iter(
-                torch.utils.data.DataLoader(
-                    GAN.data(), batch_size=1, shuffle=True
-                )
-            )
-        )[0]
-        print(latent_space_samples)
-        print(latent_space_samples.size())
-        if GPU_DEVICE:
-            latent_space_samples = latent_space_samples.cuda()
     # load the model
-    gan = GAN.load("GAN_4198.6636476")
-    # gen output
-    generated_samples = gan.generator(latent_space_samples)
-    generated_samples = generated_samples.detach()
-    # Try out the discriminator model:
-    decision = gan.discriminator(generated_samples)
-    decision = decision.detach()
-    if GPU_DEVICE:
-        generated_samples = generated_samples.cpu()
-        latent_space_samples = latent_space_samples.cpu()
-        decision = decision.cpu()
-    # INPUT
-    print("INPUT:")
-    print("--------------")
+    latent_space_samples = GAN.latent_input()
+    for file in sorted(
+        [
+            i[:-3] for i in os.listdir("./models")
+            if i.startswith("GAN_280l") and i.endswith("gen")
+        ],
+        key=lambda x: float(x.split("_")[-1])
+    ):
+        print(file)
+        gan = GAN.load("models/" + file)
+        # gen output
+        generated_samples = gan.generator(latent_space_samples)
+        generated_samples = generated_samples.detach()
+        # Try out the discriminator model:
+        decision = gan.discriminator(generated_samples)
+        decision = decision.detach()
+        if GPU_DEVICE:
+            generated_samples = generated_samples.cpu()
+            decision = decision.cpu()
+        # OUTPUT
+        print(decision)
+        plt.imshow(generated_samples[0, 0, :, :])
+        plt.figure()
+    latent_space_samples = latent_space_samples.cpu()
     plt.imshow(latent_space_samples[0, 0, :, :])
-    plt.figure()
-    # OUTPUT
-    print(generated_samples)
-    print(generated_samples.size())
-    print("--------------")
-    print(decision)
-    plt.imshow(generated_samples[0, 0, :, :])
     plt.show()
 
 if __name__ == "__main__":
--- gan/trainer.py
+++ gan/trainer.py
@@ -24,19 +24,36 @@ class Generator(nn.Module):
 
         self.model = nn.Sequential(
             nn.Conv2d(
-                in_channels=1, out_channels=12, kernel_size=3, stride=1, padding=1
+                in_channels=1,
+                out_channels=6,
+                kernel_size=24,
+                stride=3,
+                padding=4
             ),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Upsample(scale_factor=4),
+            nn.MaxPool2d(kernel_size=2, stride=1),
+            nn.Dropout2d(0.25),
             nn.Conv2d(
-                in_channels=12, out_channels=6, kernel_size=5, stride=2, padding=1
+                in_channels=6,
+                out_channels=4,
+                kernel_size=24,
+                stride=1,
+                padding=2
             ),
             nn.Dropout2d(0.25),
             nn.Conv2d(
-                in_channels=6, out_channels=1, kernel_size=2, stride=1, padding=1
+                in_channels=4,
+                out_channels=1,
+                kernel_size=10,
+                stride=2,
+                padding=2
             ),
-            # nn.ReLU(),
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=1,
+                kernel_size=7,
+                stride=1,
+                padding=1
+            ),
         )
         if GPU_DEVICE:
             self.model = self.model.cuda()
@@ -56,7 +73,7 @@ class Generator(nn.Module):
     
     def save(self, path):
         torch.save(self.model.state_dict(), path)
-
+    
 
 class Flatten(torch.nn.Module):
     def forward(self, x):
@@ -71,14 +88,14 @@ class Discriminator(nn.Module):
             nn.Conv2d(
                 in_channels=1, out_channels=4, kernel_size=3, stride=1, padding=1
             ),
-            nn.ReLU(),
+            # nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.3),
             nn.Conv2d(
                 in_channels=4, out_channels=1, kernel_size=5,
             ),
-            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.ReLU(),
             Flatten(),
             nn.Linear(25, 1),
             nn.Sigmoid(),
@@ -128,115 +145,123 @@ class GAN:
         self.discriminator.save(path + "desc")
         self.generator.save(path + "gen")
 
-    def train(self):
+    def train(self, model="generator"):
         """Train the model by iterating through the dataset
         num_epoch times, printing the duration per epoch
         """
         lr = 0.001
-        num_epochs = 150
         batch_size = 32
+        # Labels for real data: 
+        # - for discriminator, this is real images
+        # - for generator this is what we wanted the discriminator output to be
+        real_samples_labels = torch.ones(
+            (batch_size, 1), device=GPU_DEVICE
+        )
         # Init loss functions
-        # gen_loss_function = nn.CrossEntropyLoss()
-        gen_loss_function = nn.BCELoss()
-        desc_loss_function = nn.CrossEntropyLoss()
-        # Load train data
-        train_set = self.data()
-        train_loader = torch.utils.data.DataLoader(
-            train_set, batch_size=batch_size, shuffle=True
-        )
-        # Load optimizers
-        optimizer_discriminator = torch.optim.Adam(
-            self.discriminator.parameters(), lr=lr,
-        )
-        optimizer_generator = torch.optim.Adam(
-            self.generator.parameters(), lr=lr,
-        )
+        loss_function = nn.BCELoss()
+        if model == "discriminator":
+            self.generator.model.eval()
+            self.discriminator.model.train()
+            # total data is dataset * num_epochs
+            num_epochs = 10
+            # Load train data
+            train_set = self.data()
+            train_loader = torch.utils.data.DataLoader(
+                train_set, batch_size=batch_size, shuffle=True
+            )
+            # Labels for generated data, all 0
+            generated_samples_labels = torch.zeros(
+                (batch_size, 1), device=GPU_DEVICE
+            )
+            # Load optimizer
+            optimizer_discriminator = torch.optim.Adam(
+                self.discriminator.parameters(), lr=lr,
+            )
+        else: # generator
+            self.generator.model.train()
+            self.discriminator.model.eval()
+            # total data is batch_size * num_epochs
+            num_epochs = 50
+            # Load optimizer
+            optimizer_generator = torch.optim.Adam(
+                self.generator.parameters(), lr=lr,
+            )
+        loop_start = timeit.default_timer()
         # Repeat num_epoch times
         for epoch in range(num_epochs):
-            loop_start = timeit.default_timer()
             # Iterate through dataset
-            for n, (images, labels) in enumerate(train_loader):
-                if GPU_DEVICE:
-                    images = images.cuda()
-                # Data for training the discriminator
-                real_samples_labels = torch.ones(
-                    (batch_size, 1), device=GPU_DEVICE
-                )
-                latent_space_samples = torch.randn(
-                    (batch_size, 1, 28, 28), device=GPU_DEVICE
-                )
-                generated_samples = self.generator(latent_space_samples)
-                generated_samples_labels = torch.zeros(
-                    (batch_size, 1), device=GPU_DEVICE
-                )
-                # label inputs as real, fake
-                all_samples = torch.cat((images, generated_samples))
-                all_samples_labels = torch.cat(
-                    (real_samples_labels, generated_samples_labels)
-                )
-                # Training the discriminator
-                self.discriminator.zero_grad()
-                output_discriminator = self.discriminator(all_samples)
-                loss_discriminator = gen_loss_function(
-                    output_discriminator, all_samples_labels
-                )
-                loss_discriminator.backward()
-                optimizer_discriminator.step()
+            if model == "discriminator":
+                for n, (images, labels) in enumerate(train_loader):
+                    if GPU_DEVICE:
+                        images = images.cuda()
+                    # Data for training the discriminator
+                    latent_space_samples = self.latent_input(batch_size)
+                    generated_samples = self.generator(latent_space_samples)
+                    # label inputs as real, fake
+                    all_samples = torch.cat((images, generated_samples))
+                    all_samples_labels = torch.cat(
+                        (real_samples_labels, generated_samples_labels)
+                    )
+                    # Training the discriminator
+                    self.discriminator.zero_grad()
+                    output_discriminator = self.discriminator(all_samples)
+                    loss_discriminator = loss_function(
+                        output_discriminator, all_samples_labels
+                    )
+                    loss_discriminator.backward()
+                    optimizer_discriminator.step()
+            else: # generator
                 # Data for training the generator
-                latent_space_samples = torch.randn(
-                    (batch_size, 1, 28, 28), device=GPU_DEVICE
-                )
+                latent_space_samples = self.latent_input(batch_size)
                 # Training the generator
                 self.generator.zero_grad()
                 generated_samples = self.generator(latent_space_samples)
                 output_discriminator_generated = self.discriminator(
                     generated_samples
                 )
-                loss_generator = gen_loss_function(
+                loss_generator = loss_function(
                     output_discriminator_generated, real_samples_labels
                 )
                 loss_generator.backward()
                 optimizer_generator.step()
+        if model == "discriminator":
             # Show loss
             print(f"Epoch: {epoch} Loss D.: {loss_discriminator}")
+            print(timeit.default_timer() - loop_start)
+        else: # generator
             print(f"Epoch: {epoch} Loss G.: {loss_generator}")
             print(timeit.default_timer() - loop_start)
 
+    @staticmethod
+    def latent_input(batch_size=1, generated=True):
+        return torch.randn(
+            batch_size, 1, 280, 280, device=GPU_DEVICE
+        )
+
+
 def main():
-    # Train the model
-    if True:
+    # to train saved or load new model
+    if False:
         gan = GAN.load("GAN_4198.6636476", mode="train")
     else:
         gan = GAN(Discriminator(), Generator())
+    
+    # Train the models
     start = timeit.default_timer()
-    gan.train()
-    gan.save(f"GAN_cont_{timeit.default_timer()}")
-    print("Train Time:")
-    print(timeit.default_timer() - start)
-    # Try out the generator model:
-    latent_space_samples = torch.randn(1, 1, 28, 28, device=GPU_DEVICE)
-    generated_samples = gan.generator(latent_space_samples)
-    generated_samples = generated_samples.detach()
-    # Try out the discriminator model:
-    decision = gan.discriminator(generated_samples)
-    decision = decision.detach()
-    if GPU_DEVICE:
-        generated_samples = generated_samples.cpu()
-        decision = decision.cpu()
-    # show results
-    print(generated_samples)
-    print(generated_samples.size())
-    print("--------------")
-    print(decision)
-    plt.imshow(generated_samples[0, 0, :, :])
-    plt.show()
+    for i in range(11):
+        gan.train(model="discriminator")
+        gan.train(model="generator")
+        if i % (x := 1) == 0:
+            print("Train Time:")
+            print(timeit.default_timer() - start)
+            gan.save(f"models/GAN_280l_{timeit.default_timer()}")
+            start = timeit.default_timer()
 
 if __name__ == "__main__":
     print(torch.cuda.is_available())
     print(cuda.detect())
     print(torch.cuda.get_device_name(torch.cuda.current_device()))
     main()
-    # print(data())
 
 """
 TODO: 
